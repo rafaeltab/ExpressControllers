ff0fedf1c2d2765941658d98f829cdb5d214cfc7
Rafael <ikbenrafaelbieze2001@gmail.com>
2021-12-20 01:49:15 +0100

    Change methods export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { version } from "../package.json";
 export { Controller, ControllerOptions } from "./Controller";
 export { ExpressServer } from "./ExpressServer";
-export { MethodOptions, _delete, delete, get, patch, post, put } from "./Methods";
+export * as Methods from "./Methods";
 export { RequestParameterOptions, requestParameter } from "./RequestParameter";
